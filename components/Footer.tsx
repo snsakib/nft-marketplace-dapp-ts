0b9f3c1534f93555b4637d5c9e09a461603b5592
snsakib <hello@snsakib.com>
2023-07-02 16:58:40 +0600

    styling footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <div className="text-center">
+    <div className="text-center border-t p-5">
       Designed & Developed by &nbsp;
       <a href="https://www.snsakib.com" target="_blank" rel="noopener noreferrer" className="text-blue-500 text-xl italic">S. N. Sakib</a>
     </div>
