32c6650ab3ab593b84823895acfefb7e6dd27662
snsakib <hello@snsakib.com>
2023-07-02 16:57:29 +0600

    modify page layout

--- app/mint-nft/page.tsx
+++ app/mint-nft/page.tsx
@@ -1,7 +1,7 @@
 export default function MintNFT() {
   return (
-    <main>
+    <>
       Mint NFT
-    </main>
+    </>
   )
 }
--- app/my-nft/page.tsx
+++ app/my-nft/page.tsx
@@ -1,7 +1,7 @@
 export default function MyNFT() {
   return (
-    <main>
+    <>
       My NFT
-    </main>
+    </>
   )
 }
