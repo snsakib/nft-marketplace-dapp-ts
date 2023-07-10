f7643981f02668c1ee999a98fb17bdefc98bb2f4
snsakib <hello@snsakib.com>
2023-07-11 02:41:42 +0600

    fix header layout

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,10 +5,10 @@ import { Web3Button } from "@web3modal/react";
 
 export default function Header() {
   return (
-    <header className="flex flex-col min-[800px]:flex-row justify-between items-center py-5 px-10 border-b">
+    <header className="flex flex-col min-[825px]:flex-row justify-between py-5 px-10 border-b">
       <div className="flex flex-row items-center mb-5 md:lg-0 md:mr-5">
         <Image src="/logo.svg" width={40} height={40} alt="Logo" />
-        <h1 className="ml-5 text-xl min-[500px]:text-2xl">NFT Marketplace</h1>
+        <h1 className="ml-5 text-xl font-bold min-[500px]:text-2xl">NFT Marketplace</h1>
       </div>
       <div className="flex flex-col items-center max-[900px]:flex-1 min-[500px]:flex-row min-[500px]:justify-between md:text-xl">
         <div className="flex justify-between flex-1 text-md font-bold text-sky-500">
