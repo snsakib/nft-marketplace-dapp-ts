82605490c1d947fc4f20f7d2254a935312c51b54
snsakib <hello@snsakib.com>
2023-07-11 01:57:06 +0600

    fix header layout for mobile

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,20 +6,22 @@ import { Web3Button } from '@web3modal/react'
 export default function Header() {
 
   return (
-    <header className='flex flex-row justify-between items-center py-5 px-10 border-b'>
-      <div className='flex flex-row items-center'>
+    <header className='flex flex-col md:flex-row justify-between py-5 px-10 border-b'>
+      <div className='flex flex-row items-center mb-5 md:lg-0'>
         <Image
           src='/logo.svg'
-          width={50}
-          height={50}
+          width={40}
+          height={40}
           alt='Logo'
         />
-        <h1 className='ml-5 text-2xl'>NFT Marketplace</h1>
+        <h1 className='ml-5 text-xl'>NFT Marketplace</h1>
       </div>
-      <div className='text-xl'>
-        <Link href='/' className='px-3'>Home</Link>
-        <Link href='/mint-nft' className='px-3'>Mint NFT</Link>
-        <Link href='/my-nft' className='px-3'>My NFT</Link>
+      <div className='flex justify-between text-md font-bold text-sky-500'>
+        <Link href='/'>Home</Link>
+        <Link href='/mint-nft'>Mint NFT</Link>
+        <Link href='/my-nft'>My NFT</Link>
+      </div>
+      <div className='pt-5 text-center'>
         <Web3Button />
       </div>
     </header>
